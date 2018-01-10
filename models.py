ca34120c1c9e9c7b0770a16b9a9e9a74e3459bb1
Soumyajit Dutta <sanudatta11@gmail.com>
2018-01-10 19:21:42 +0530

    Initial

--- models.py
+++ models.py
@@ -9,11 +9,19 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(254),nullable=False)
+    password = db.Column(db.String(400),nullable=False)
+
+    membertype = db.Column(db.Integer,default=1) # 1 = Ordinary Member, Sustaining Member
+    persontype = db.Column(db.Integer,default=2) #1 = Legal Person , 2 = Natural Person
+
+    status = db.Column(db.Integer,default=3) #1 = Active,2=Inactive,3=Leaked,4=Deceased
+
+    company = db.Column(db.String(300),nullable=True,default="No Company")
 
     firstname = db.Column(db.String(100),nullable=False)
     lastname = db.Column(db.String(100),nullable=False)
 
-    bday = db.Column(db.DateTime)
+    bday = db.Column(db.DateTime,nullable=True)
 
     road = db.Column(db.String(500))
     town = db.Column(db.String(400))
@@ -21,7 +29,10 @@ class User(db.Model):
 
     phone = db.Column(db.Integer)
     mobile = db.Column(db.Integer,nullable=False)
+
+    fee = db.Column(db.Integer, nullable=False, default=MIN_FEE)
     created_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.datetime.utcnow)
 
     bankdetails = relationship("Bankdetails", uselist=False, back_populates="User")
 

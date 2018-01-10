ed40b1548a15c047b5d741216ba59dbfdd9fe611
JeanClaudeMerlot <JeanClaudeMerlot@users.noreply.github.com>
2018-01-10 23:50:27 +0100

    Update models.py
    added Models for later use

--- app/models.py
+++ app/models.py
@@ -16,6 +16,7 @@ class User(db.Model):
     first_fee = db.Column(db.Integer,nullable=False)
     monthlyfee = db.Column(db.Integer, nullable=False)
     status = db.Column(db.Integer,default=2) #1 = Active,2=Inactive,3=Leaked,4=Deceased
+    created_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
     #Company
     company = db.Column(db.String(300),nullable=True,default="No Company") #No Company for Natural Person Valid for persontype 1
@@ -29,11 +30,20 @@ class User(db.Model):
     postcode = db.Column(db.Integer,nullable=False)
     phone = db.Column(db.Integer)
     mobile = db.Column(db.Integer,nullable=False)
+    
+    def avatar(self, size):   # https://en.gravatar.com/site/implement/ and https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-vi-profile-page-and-avatars
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
-    created_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    
     updated_at = db.Column(db.DateTime, onupdate=datetime.datetime.utcnow)
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow) #on website
 
     bankdetails = db.relationship('Bankdetails', backref='user',lazy='dynamic')
+    
+    #
+    
 
 class Bankdetails(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -47,7 +57,49 @@ class Bankdetails(db.Model):
     sepa_date = db.Column(db.DateTime,default=datetime.datetime.utcnow)
     sepa_ref = db.Column(db.String(200),nullable=False)
     user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    
 
+    
+##############  Blog/News-Entrys  ##############  
+# class Blogentry(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    date_created = db.Column(db.DateTime,default=datetime.datetime.utcnow)
+#    title = db.Column(db.String(200), nullable=False)
+#    content = db.Column(db.String(200), nullable=False)
+#    user_id_author = db.Column(db.Integer,db.ForeignKey('user.id'))
+   
+##############  Trees and Reports  ##############  
+#  class Tree(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    tpye = db.Column(db.String(200)
+#    age = db.Column(db.Integer)
+#    location = db.Column(db.String(200)
+#    date_last_pruned = db.Column(db.DateTime,default=datetime.datetime.utcnow)
+#    date_last_picked = db.Column(db.DateTime,default=datetime.datetime.utcnow)
+#    
+#    
+#  class Pruningreport(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    title = db.Column(db.String(200), nullable=False)
+#    body = db.Column(db.String(2000))                    
+#    date_pruned = db.Column(db.DateTime,default=datetime.datetime.utcnow)
+#    date_submitted = db.Column(db.DateTime,default=datetime.datetime.utcnow)
+#    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))                         
+#    tree_id = db.Column(db.Integer,db.ForeignKey('tree.id'))
+#    
+#  class Pickingreport(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    title = db.Column(db.String(200), nullable=False)
+#    body = db.Column(db.String(2000))
+#    weight_picked = db.Column(db.Integer)
+#    date_picked = db.Column(db.DateTime,default=datetime.datetime.utcnow)
+#    date_submitted = db.Column(db.DateTime,default=datetime.datetime.utcnow)
+#    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))                         
+#    tree_id = db.Column(db.Integer,db.ForeignKey('tree.id'))
+    
+
+    
+    
 # class subscription(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
 #     type = db.Column(db.Integer,default=1) #1 = Ordinary Member ,2 = Conveyer Member
